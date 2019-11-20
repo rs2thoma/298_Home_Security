7bd2dd41d7e8595b0b8e790362a7552b1d19575e
Riley Thomas <rileythomas_11@hotmail.com>
2019-11-20 16:48:14 -0500

    changed ultra pins and ports to enum

--- src/ultrasonic.h
+++ src/ultrasonic.h
@@ -8,11 +8,30 @@
 #ifndef ULTRASONIC_H_
 #define ULTRASONIC_H_
 
+#include "driverlib/driverlib.h"
 #include "stdint.h"
 
 #define ULTRA_TRIG_DELAY    20 //10us
 
-#define ULTRA_TRIG_PORT     GPIO_PORT_P2
+typedef enum
+{
+    ULTRA_TRIG_PORT = GPIO_PORT_P2,
+    ULTRA1_ECHO_PORT = GPIO_PORT_P8,
+    ULTRA2_ECHO_PORT = GPIO_PORT_P5,
+    ULTRA3_ECHO_PORT = GPIO_PORT_P2,
+    ULTRA4_ECHO_PORT = GPIO_PORT_P8
+} ULTRA_PORTS;
+
+typedef enum
+{
+    ULTRA_TRIG_PIN = GPIO_PIN7,
+    ULTRA1_ECHO_PIN = GPIO_PIN0,
+    ULTRA2_ECHO_PIN = GPIO_PIN1,
+    ULTRA3_ECHO_PIN = GPIO_PIN5,
+    ULTRA4_ECHO_PIN = GPIO_PIN2
+} ULTRA_PINS;
+
+/*#define ULTRA_TRIG_PORT     GPIO_PORT_P2
 #define ULTRA_TRIG_PIN      GPIO_PIN7
 #define ULTRA1_ECHO_PORT    GPIO_PORT_P8
 #define ULTRA1_ECHO_PIN     GPIO_PIN0
@@ -21,7 +40,7 @@
 #define ULTRA3_ECHO_PORT    GPIO_PORT_P2
 #define ULTRA3_ECHO_PIN     GPIO_PIN5
 #define ULTRA4_ECHO_PORT    GPIO_PORT_P8
-#define ULTRA4_ECHO_PIN     GPIO_PIN2
+#define ULTRA4_ECHO_PIN     GPIO_PIN2*/
 
 void ultra_Trigger(void);
 uint16_t ultra1_getDistance(void);
